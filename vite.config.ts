3d156df4482e30d9494e11e11a3487d4a6e42d9c
DESKTOP-VO6E52I\rn134 <rn1349@naver.com>
2022-02-21 21:38:57 +0900

    임시 서버 포트 변경

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 18080,
+  },
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
