5f87b278dc20b35e084a4dc2e6695736e7029afa
JHG\rn134 <rn1349@naver.com>
2022-04-26 22:39:24 +0900

    안되는 이유가 뭐여

--- src/plugins/axios/axios.ts
+++ src/plugins/axios/axios.ts
@@ -3,7 +3,7 @@ import { useStore } from 'vuex'
 
 import TokenService from '@/services/token/token-service'
 
-axios.defaults.baseURL = 'http://localhost:18090'
+axios.defaults.baseURL = 'http://hungu.synology.me:18090'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 axios.defaults.headers.post['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
