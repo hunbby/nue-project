292148ba48c3f41c74e2afc0c38ea0475f0a459f
JHG\rn134 <rn1349@naver.com>
2022-04-28 21:02:18 +0900

    cors 테스트 마지막!

--- src/plugins/axios/axios.ts
+++ src/plugins/axios/axios.ts
@@ -4,6 +4,7 @@ import { useStore } from 'vuex'
 import TokenService from '@/services/token/token-service'
 
 axios.defaults.baseURL = 'http://hungu.synology.me:18090/cms'
+// axios.defaults.baseURL = 'http://localhost:18090/cms'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 axios.defaults.withCredentials = true
